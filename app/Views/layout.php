778433dc95e9e5f1c807b233fb98b28e956ea585
anza1922 <111202415792@mhs.dinus.ac.id>
2026-05-04 23:26:24 +0700

    Update desain layout

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -1,6 +1,8 @@
 <?= $this->include('components/header') ?>
 <?= $this->include('components/sidebar') ?>
 <?= $this->include('components/topbar') ?>
+<div class="container-fluid px-4">
 <?= $this->renderSection('content') ?>
+</div>
 <?= $this->include('components/footer') ?>
-<?= $this->include('components/script') ?>
+<?= $this->include('components/script') ?>
